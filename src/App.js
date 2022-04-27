16aba95b14d636deeb0fc788eff8a74715c3ef6a
Patrik <havrilap@gmail.com>
2022-04-27 19:35:00 +0200

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App () {
               <Link to='/'>Home</Link>
             </li>
             <li>
-              <Link to='/about'>About</Link>
+              <Link to='/about'>TEST</Link>
             </li>
             <li>
               <Link to='/users'>Users</Link>
